7c2da4e621216114a269aca3cf006860f6c6e0da
Grigore Stefan <g_stefan@yahoo.com>
2021-08-09 15:41:27 +0300

    Update

---
+++ build/msvc.version-dependency-update.cmd
@@ -0,0 +1,8 @@
+@echo off
+rem Public domain
+rem http://unlicense.org/
+rem Created by Grigore Stefan <g_stefan@yahoo.com>
+
+echo -^> version-dependency-update vendor-zlib
+
+xyo-cc --update-version-dependency vendor-zlib
